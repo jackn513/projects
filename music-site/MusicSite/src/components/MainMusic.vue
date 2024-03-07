2f6eb5c9f3e7398d437f1f2e2946be1da3fb29f3
Jack Nichols <jack.nichols348@gmail.com>
2024-03-07 16:00:34 -0500

    Here

---
+++ music-site/MusicSite/src/components/MainMusic.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <h1>Hello</h1>
+    </header>
+</template>
+
+<script>
+
+export default defineComponent({
+    data() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
